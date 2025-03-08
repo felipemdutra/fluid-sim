1d9eff7c3c87b59472ac20ae7c3503f338014ba7
Felipe Dutra <felipemd.dutra@gmail.com>
2025-03-08 15:54:49 -0300

    feat: add simple Window struct Added window related functions like windowClear, pollEvents, swapBuffers, etc...

---
+++ include/window.h
@@ -0,0 +1,21 @@
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+#include <string>
+
+struct Window {
+    GLFWwindow* nativeWindow = nullptr;
+
+    int width = 800, height = 600;
+    std::string title;
+};
+
+Window* createWindow(const float width, const float height, const std::string title);
+void destroyWindow(Window* window);
+
+int initWindow(Window* window);
+
+bool windowShouldClose(Window* window);
+void swapBuffers(Window* window);
+void pollEvents();
+void windowClear(const float r, const float g, const float b, const float a);
+
