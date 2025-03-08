55b31e0bcd2aff07fa67eb72734d68d93ff71ee8
Felipe Dutra <felipemd.dutra@gmail.com>
2025-03-08 15:51:26 -0300

    fix: class redefinition Added 'modelMatrix' member to Particle struct Added header guards Fixed class redefinition bug

--- include/particle.h
+++ include/particle.h
@@ -1,14 +1,22 @@
+#ifndef PARTICLE_H
+#define PARTICLE_H
+
 #include <glm/glm.hpp>
 
-#include "mesh.h"
+class Mesh;
 
 struct Particle {
     glm::vec2 pos = glm::vec2();
     glm::vec2 vel = glm::vec2();
     glm::vec2 acc = glm::vec2();
+
+    glm::mat4 modelMatrix = glm::mat4(1.0f);
+
     float mass = 1.0f;
     float density = 1.0f;
 
     static Mesh mesh;
 };
+
+#endif
 
