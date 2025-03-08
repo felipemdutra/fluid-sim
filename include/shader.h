4ddc6f24bc5ab47269584fb12be9832898db20c5
Felipe Dutra <felipemd.dutra@gmail.com>
2025-03-08 11:25:52 -0300

    feat: add Shader class

---
+++ include/shader.h
@@ -0,0 +1,22 @@
+#include <glm/glm.hpp>
+#include <string>
+
+class Shader {
+    private:
+        unsigned int id;
+
+        void checkCompileErrors(unsigned int shader, std::string type);
+        void checkLinkErrors(unsigned int shaderProgram);
+
+    public:
+        Shader(const char* vertexPath, const char* fragmentPath);
+
+        void use();
+
+        void setInt(const std::string &name, int value) const;
+        void setFloat(const std::string &name, float value) const;
+
+        void setVec3(const std::string &name, glm::vec3 value) const;
+        void setMat4(const std::string &name, glm::mat4 value) const;
+};
+
