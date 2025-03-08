d74bdc102af6a24639fd8f4b6239c14278a977a5
Felipe Dutra <felipemd.dutra@gmail.com>
2025-03-08 15:52:14 -0300

    feat: initialize window and clear screen with new color

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
-#include "particle.h"
+#include "window.h"
+
+#define WIDTH 1000
+#define HEIGHT 800 
 
 int main() {
-    Particle p1;
+    Window* window = createWindow(WIDTH, HEIGHT, "Hello, World!");
+    if (!window) {
+        return 1;
+    }
+
+    if (initWindow(window) < 0) {
+        return 1;
+    }
+
+    while (!windowShouldClose(window)) {
+        windowClear(0.2f, 0.3f, 0.3f, 1.0f);
+
+        swapBuffers(window);
+        pollEvents();
+    }
+
+    destroyWindow(window);
+    return 0;
 }
 
